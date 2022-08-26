e8d56593d143a1d17953234b9cac38b81e0c431a
benderill <40989185+benderill@users.noreply.github.com>
2022-08-26 13:09:53 +0200

    Update run.py
    Deleted redundant options. 
    Updated file names

--- run.py
+++ run.py
@@ -5,13 +5,12 @@ import datetime
 
 nn_trained = 'no'
 
-dataset = Path("./Dataset/one_dionde_4_temp_200000.h5")
-exp_path = Path("./Experimental_data/one_dionde_4_temp_781.h5")
-sim_name = 'one_diode_781'
+dataset = Path("./Dataset/one_dionde_4_temp_30000.h5")
+exp_path = Path("./Experimental_data/one_diode_4_temp_781.h5")
+sim_name = 'one_diode'
 exp = 'one'
 if nn_trained == 'no':
     timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    exp = 'one'
     dir_path, reg_path,train_path, scaler_path = NN_training.main(sim_name, timestamp, exp, dataset)
     print('reg_path', reg_path, train_path, scaler_path)
     print('train_path', train_path)
@@ -21,6 +20,5 @@ elif nn_trained == 'yes':
     reg_path = dir_path/('20220609-051420_one_diode_4_temp_781_trained_model.h5')
     train_path = dir_path/('20220609-051420_one_diode_4_temp_781_train_test.h5')
     scaler_path = dir_path/('20220609-051420scaler.joblib')
-    exp_path = Path("./Experimental_data/one_dionde_4_temp_781.h5")
-
+    
 bayes.main(dir_path, sim_name, reg_path, train_path, scaler_path, exp_path, exp)
