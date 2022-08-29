7334405f8def3e7163cd5a9991da666ea63ee373
benderill <40989185+benderill@users.noreply.github.com>
2022-08-29 19:32:19 +0200

    Add files via upload

---
+++ submit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH -o batch.sh.out-%j
+#SBATCH --gres=gpu:volta:1
+#SBATCH -c 20
+
+# Loading the required module
+source /etc/profile
+module load anaconda/2021a
+
+# Run the script
+python run.py
